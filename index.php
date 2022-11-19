1bf390c7c9dad03fd42461fead64c0aeb468fd69
jhongo <jhon_gom22@outlook.com>
2022-11-18 22:07:40 -0500

    Start style with tailwind

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Formulario</title>
 </head>
 <body>
@@ -30,7 +31,7 @@
     <strong>Tu lenguaje favorito es </strong>:<?php echo $rdgLenguaje ?>
     <?php } ?>
 
-    <form action="index.php" method="post">
+    <form class="bg-[#606c38] w-1/3 " action="index.php" method="post">
 
         <label for="">Nombre</label>
         <br/>
