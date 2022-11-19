ff98264a0c1acda3c8d3e353f78f3b0014911158
jhongo <jhon_gom22@outlook.com>
2022-11-18 22:20:21 -0500

    Final style radio in the form

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
 
-    $txtNombre="";
-    $rdgLenguaje="";
-    if ($_POST) {
-        
-        $txtNombre=(isset($_POST['txtNombre'])) ? $_POST['txtNombre'] : "" ;
-        $rdgLenguaje=(isset($_POST['txtNombre'])) ? $_POST['lenguaje'] : "" ;
-        
-        
-    }
- 
- 
- ?>
+$txtNombre = "";
+$rdgLenguaje = "";
+if ($_POST) {
+
+    $txtNombre = (isset($_POST['txtNombre'])) ? $_POST['txtNombre'] : "";
+    $rdgLenguaje = (isset($_POST['txtNombre'])) ? $_POST['lenguaje'] : "";
+}
+
+
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,29 +23,60 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Formulario</title>
 </head>
+
 <body>
-    <?php if($_POST){  ?>
-    <strong>Hola </strong>:<?php echo $txtNombre ?>
-    <br/>
-    <strong>Tu lenguaje favorito es </strong>:<?php echo $rdgLenguaje ?>
+    <?php if ($_POST) {  ?>
+        <strong>Hola </strong>:<?php echo $txtNombre ?>
+        <br />
+        <strong>Tu lenguaje favorito es </strong>:<?php echo $rdgLenguaje ?>
     <?php } ?>
 
-    <form class="bg-[#606c38] w-1/3 " action="index.php" method="post">
+    <form class="bg-[#f8edeb] w-1/3 h-1/3 m-12 " action="index.php" method="post">
 
         <label for="">Nombre</label>
-        <br/>
-        <input value="<?php echo $txtNombre ?>" type="text" name="txtNombre" placeholder="Ingresar dato" id=""> <br/>
-        <label for="">Lenguaje de preferencia</label> 
-        <br/> PHP: <input type="radio" <?php echo ($rdgLenguaje=="php" ?"checked":"") ?> name="lenguaje" value="php" id=""> <br/>
-        <br/> HTML: <input type="radio" <?php echo ($rdgLenguaje=="html" ?"checked":"") ?> name="lenguaje" value="html" id=""><br/>
-        <br/> DART: <input type="radio" <?php echo ($rdgLenguaje=="dart" ?"checked":"") ?> name="lenguaje" value="dart" id=""><br/>
+        <br />
+        <input value="<?php echo $txtNombre ?>" type="text" name="txtNombre" placeholder="Ingresar dato" id=""> <br />
+        <label for="">Lenguaje de preferencia</label>
+
+        <!-- <br /> PHP: <input type="radio" <?php echo ($rdgLenguaje == "php" ? "checked" : "") ?> name="lenguaje" value="php" id=""> <br />
+        <br /> HTML: <input type="radio" <?php echo ($rdgLenguaje == "html" ? "checked" : "") ?> name="lenguaje" value="html" id=""><br />
+        <br /> DART: <input type="radio" <?php echo ($rdgLenguaje == "dart" ? "checked" : "") ?> name="lenguaje" value="dart" id=""><br /> -->
+
+        <div class="flex my-4 mx-1.5">
+            <div>
+
+                <div class="form-check">
+                    <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" <?php echo ($rdgLenguaje == "Dart" ? "checked" : "") ?> value="Dart" type="radio" name="lenguaje" id="">
+                    <label class="form-check-label inline-block text-gray-800" for="">
+                        Dart
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" <?php echo ($rdgLenguaje == "Php" ? "checked" : "") ?> value="Php" type="radio" name="lenguaje" id="">
+                    <label class="form-check-label inline-block text-gray-800" for="lenguaje2">
+                        PHP
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"<?php echo ($rdgLenguaje == "Java" ? "checked" : "") ?> value="Java" type="radio" name="lenguaje" id="">
+                    <label class="form-check-label inline-block text-gray-800" for="lenguaje2">
+                        Java
+                    </label>
+                </div>
+
+            </div>
+        </div>
 
 
 
 
-        <input type="submit" value="Enviar">
+        <!-- <input type="submit" value="Enviar"> -->
+        <button type="submit" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Enviar</button>
 
     </form>
-    
+
 </body>
+
 </html>
