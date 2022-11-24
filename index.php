27934851779a472618a1632f3fa8090b5362f4a2
jhongo <jhon_gom22@outlook.com>
2022-11-23 22:57:34 -0500

    Add data redundante in textarea

--- index.php
+++ index.php
@@ -138,7 +138,10 @@ if ($_POST) {
 
         <label for="chat" class="sr-only">Tienes alguna duda</label>
         <div class="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-            <textarea name="txtAreaComment" id="chat" rows="1" class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+            <textarea name="txtAreaComment" id="chat" rows="1" class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message...">
+
+            <?php echo $txtAreaComment; ?>
+            </textarea>
         </div>
 
 
